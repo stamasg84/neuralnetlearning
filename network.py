5e37ac67f7a9addf5a5e5a6ab3a5cbab2de75206
Tamás Selmeczi <tamas.selmeczi@gsgroup.hu>
2022-12-13 12:11:44 +0100

    SGD method partitioning data

--- network.py
+++ network.py
@@ -5,10 +5,10 @@ class Network:
         self.sizes = sizes
         self.numLayers = len(sizes)
 
-        rng = np.random.default_rng()
+        self.rng = np.random.default_rng()
 
-        self.biases = [rng.standard_normal(layerSize) for layerSize in sizes[1:]]
-        self.weights = [rng.standard_normal((layerSize, previousLayerSize)) for previousLayerSize, layerSize in zip(sizes, sizes[1:])]
+        self.biases = [self.rng.standard_normal(layerSize) for layerSize in sizes[1:]]
+        self.weights = [self.rng.standard_normal((layerSize, previousLayerSize)) for previousLayerSize, layerSize in zip(sizes, sizes[1:])]
 
     @staticmethod
     def sigmoid(z):
@@ -23,12 +23,53 @@ class Network:
         
         return result
 
-sizes = [5,3,2]
+    def SGD(self, trainingInput, miniBatchSize, numberOfEpochs, eta, testFunction = None):
+        '''Executes stochastic gradient descent on the specified training input(Tuples of training data and expected result). The sample size to use
+        when calculating the cost function is miniBatchSize. An epoch consists of a weight and bias adjustment(learning) on each mini batch that comprises the trainingInput.
+        In other words an epoch consumes all entries in trainingInput once, having put each of them in a miniBatch and using that miniBatch to approximate the cost function and do 
+        a single weight and bias adjustment. eta is the learning rate(a greek letter) testFunction is an optional function that will be called at the end of each epoch with the network
+        as a parameter. Can be used to do some evaluation of network performance'''
+        for i in range(numberOfEpochs):
+            shuffledTrainingInput = trainingInput.copy()
+            self.rng.shuffle(shuffledTrainingInput)
+            miniBatches = [shuffledTrainingInput[k:k+miniBatchSize] for k in range(0, len(trainingInput), miniBatchSize)]
+
+            for miniBatch in miniBatches:
+                self.updateMiniBatch(miniBatch, eta)
+            
+            if testFunction:
+                print('Test result for epoch:')
+                testFunction(self)
+            
+            print('Epoch {i} completed.'.format(i=i+1))
+    
+    def updateMiniBatch(self, miniBatch, eta):
+        print(miniBatch)
+        
+
+def getNetworkResultForTestData(network, testData):
+    '''Returns the index of the neuron that yielded the highest result from the output layer of the network (for digit recognition this equals to the recognized digit)'''
+    return np.argmax(network.feedforward(testData))
+
+def testNetwork(network, input):
+    '''Assuming input is a list of tuples of inputData-expectedResult pairs, it prints a diagnostic on how many correct results the network yielded on the input'''
+    resultsAndExpectations = [ (getNetworkResultForTestData(network, testData), expectedResult) for (testData, expectedResult) in input ]
+    totalCorrectResults = sum(result==expectation for (result,expectation) in resultsAndExpectations)
+    print('Number of correct results:', totalCorrectResults)
+
+
+sizes = [3,3,2]
 a = Network(sizes)
 
-print(a.biases)
-print(a.weights)
+trainingInput = [(np.array((1, 2, 3)), 1), 
+                 (np.array((3, 4, 5)), 9), 
+                 (np.array((2, 2, 2)), 7), 
+                 (np.array((6, 1, 1)), 2), 
+                 (np.array((1, 2, 5)), 2), 
+                 (np.array((3, 4, 5)), 9), 
+                 (np.array((1, 2, 3)), 1), 
+                 (np.array((3, 4, 5)), 9)]
 
-print(a.feedforward(np.array((1, 2, 3, 4, 5))))
+testData = [ (np.array((1, 2, 3)), 1), (np.array((3, 4, 5)), 9) ]
 
-print('Hello GIT from private machine!')
+a.SGD(trainingInput, 2, 3, 0.1, lambda n: testNetwork(n, testData))
