80b883830057f4945cf1c494cefb601695bb0161
Tamás Selmeczi <tamas.selmeczi@gsgroup.hu>
2022-12-15 13:53:09 +0100

    Prevent image viewer collision

--- viewmnistimage.py
+++ viewmnistimage.py
@@ -1,14 +1,12 @@
-'''Execute this file in a REPL then call showImage() with an arbitrary number up to 49999 to see the corresponding mnist test image'''
+'''Execute this file in a REPL then call showImage() with an arbitrary number up to 9999 to see the corresponding mnist test image'''
 
 import numpy as np
 from mnist_loader import MnistData
 
 from PIL import Image
 
-mnistData = MnistData()
-pixelArrays = list([pixelArray for (pixelArray, expectedResult) in mnistData.trainingData])
-
-def showImage(index):
+def showImage(index, mnistData : MnistData):
+    pixelArrays = list([pixelArray for (pixelArray, expectedResult) in mnistData.validationData])
     pixelArray = pixelArrays[index]
     scaledArray = pixelArray * 255
     scaledArray.resize((28,28))
